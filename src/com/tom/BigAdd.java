0ef4f3856793b3356305a8c2f96941b327d64118
User <User@ES301N5>
2020-10-20 15:24:55 +0800

    hello

---
+++ src/com/tom/BigAdd.java
@@ -0,0 +1,14 @@
+package com.tom;
+
+public class BigAdd {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < ( 5 - i ) ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
